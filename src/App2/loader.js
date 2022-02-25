c6ac94cdafc48aa45e7841a5a962914f0bac4a17
jossnmb <jossnmb@gmail.com>
2022-02-25 15:02:45 -0500

    1 file modified

--- src/App2/loader.js
+++ src/App2/loader.js
@@ -1,6 +1,36 @@
 // import { useEffect, useState } from 'react';
 import axios from 'axios';
 
+//this successfully runs on button press, but I have no idea how to return the dogUrl back to PurchasePage
+async function DogLoader(retryCnt, max) {
+  if (retryCnt >= max) throw new Error(`Failed retrying ${retryCnt} times`);
+  let dogUrl;
+  await axios
+    .get('https://random.dog/woof')
+    // if resolved
+    .then((res) => {
+      // no mp4s allowed as img
+      if (res.data.includes('mp4')) {
+        console.log('reloading image!');
+        retryCnt++;
+        DogLoader(retryCnt);
+      } else {
+        console.log('loaded image success!');
+      }
+      dogUrl = 'https://random.dog/' + res.data;
+    })
+    .catch((e) => {
+      console.log(e);
+    })
+    .then(() => {
+      console.log(dogUrl);
+      return dogUrl;
+    });
+}
+
+export { DogLoader };
+
+//old code attempts
 //want to return dogImg after done loading
 // export default function Loader() {
 //   const [loaded, setLoaded] = useState(false);
@@ -58,32 +88,3 @@ import axios from 'axios';
 //     console.log(e);
 //   }
 // }
-
-//this successfully runs on button press
-async function DogLoader(retryCnt, max) {
-  if (retryCnt >= max) throw new Error(`Failed retrying ${retryCnt} times`);
-  let dogUrl;
-  await axios
-    .get('https://random.dog/woof')
-    // if resolved
-    .then((res) => {
-      // no mp4s allowed as img
-      if (res.data.includes('mp4')) {
-        console.log('reloading image!');
-        retryCnt++;
-        DogLoader(retryCnt);
-      } else {
-        console.log('loaded image success!');
-      }
-      dogUrl = 'https://random.dog/' + res.data;
-    })
-    .catch((e) => {
-      console.log(e);
-    })
-    .then(() => {
-      console.log(dogUrl);
-      return dogUrl;
-    });
-}
-
-export { DogLoader };
