91c226e7995316732bde6251539ddbafd900db5e
jossnmb <jossnmb@gmail.com>
2022-04-18 15:00:33 -0400

    1 file modified

--- src/App8/App.jsx
+++ src/App8/App.jsx
@@ -1,5 +1,9 @@
 import './app.css';
 import Home from './pages/home/Home';
+import List from './pages/list/List';
+import Login from './pages/login/Login';
+import New from './pages/new/New';
+import Single from './pages/single/Single';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export default function App() {
@@ -7,7 +11,15 @@ export default function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="users">
+              <Route index element={<List />} />
+              <Route path=":userID" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+          </Route>
           <Route
             path="*"
             element={
