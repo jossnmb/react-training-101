0a07e36000432db6ecbb2b36149145517c2c5069
jossnmb <jossnmb@gmail.com>
2022-04-19 11:26:19 -0400

    1 file modified

--- src/App8/components/sidebar/Sidebar.jsx
+++ src/App8/components/sidebar/Sidebar.jsx
@@ -1,4 +1,11 @@
 import './sidebar.scss';
+import {
+  DashboardIcon,
+  AccountCircleIcon,
+  InventoryIcon,
+  BallotIcon,
+  LocalShippingIcon,
+} from '@mui/icons-material/Dashboard';
 
 export default function Sidebar() {
   return (
@@ -6,26 +13,46 @@ export default function Sidebar() {
       <div className="top">
         <span className="logo">jossadmin</span>
       </div>
-      <hr/>
+      <hr />
       <div className="center">
         <ul>
           <li>
+            <DashboardIcon />
             <span>Dashboard</span>
           </li>
-        </ul>
-        <ul>
           <li>
-            <span>Dashboard</span>
+            <AccountCircleIcon />
+            <span>Users</span>
           </li>
-        </ul>
-        <ul>
           <li>
-            <span>Dashboard</span>
+            <InventoryIcon />
+            <span>Products</span>
           </li>
-        </ul>
-        <ul>
           <li>
-            <span>Dashboard</span>
+            <BallotIcon />
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingIcon />
+            <span>Delivery</span>
+          </li>
+          <li>
+            <span>Stats</span>
+          </li>
+          <li>
+            <span>Notifications</span>
+          </li>
+          <li>
+            <span>Logs</span>
+          </li>
+          <li>
+            <span>Settings</span>
+          </li>
+          <li>
+            <span>Profile</span>
+          </li>
+          <li>
+            <span>Logout</span>
           </li>
         </ul>
       </div>
